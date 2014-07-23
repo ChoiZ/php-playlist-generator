47abccddefad5b4ef42f5f761596626582f41c88
François LASSERRE <choiz@me.com>
2014-07-23 09:10:27 +0200

    Update generate.php
    Replace switch / case by if / else if

--- generate.php
+++ generate.php
@@ -2,8 +2,8 @@
 /*
  * Playlist generator
  *
- * @version 2012-07-06T22:11:17Z (ISO-8601)
- * @author François LASSERRE <choiz@me.com>·
+ * @version 2014-07-23
+ * @author François LASSERRE <choiz@me.com>
  * @license GNU GPL {@link http://www.gnu.org/licenses/gpl.html}
  */
 
@@ -26,55 +26,46 @@ $file = new stdClass;
 $file->name = $radio->station;
 $file->ext = $available_format[1];
 
-if(isset($_GET['ext']) && $_GET['ext'] && in_array(strtolower($_GET['ext']),$format)) {
+if (isset($_GET['ext']) && $_GET['ext'] && in_array(strtolower($_GET['ext']),$format)) {
     $file->ext = strtolower($_GET['ext']);
 }
 
-switch($file->ext) {
-    case 'asx':
-        header("Content-Type: audio/x-ms-asf");
-        header("Content-Disposition: attachment; filename=$file->name.$file->ext");
-        echo "<ASX Version=\"3.0\">\n<PARAM name=\"HTMLView\" value=\"$radio->website_url\" />\n";
-        foreach($radio->stream_url as $url) {
-            echo "<ENTRY>\n<REF HREF=\"http://$url\" />\n</ENTRY>\n";
-        }
-        echo "<Title>$radio->stream_title</Title>\n</ASX>";
-        break;
-    case 'm3u':
-        header("Content-Type: audio/x-mpegurl");
-        header("Content-Disposition: attachment; filename=$file->name.$file->ext");
-        echo "#EXTM3U";
-        foreach($radio->stream_url as $url) {
-            echo "\n#EXTINF:-1, $radio->stream_title\n$url";
-        }
-        break;
-    case 'pls':
-        header("Content-Type: audio/x-scpls");
-        header("Content-Disposition: attachment; filename=$file->name.$file->ext");
-        echo "[playlist]\nNumberOfEntries=".count($radio->stream_url)."\n";
-        $i=0;
-        foreach($radio->stream_url as $url) {
-            $i++;
-            echo "\nFile$i=http://$url\nTitle$i=$radio->stream_title\nLength$i=-1\n";
-        }
-        echo "\nVersion=2";
-        break;
-    case 'qtl':
-        header("Content-Type: application/x-quicktimeplayer");
-        header("Content-Disposition: attachment; filename=$file->name.$file->ext");
-        echo "<?xml version=\"1.0\"?>\n<?quicktime type=\"application/x-quicktime-media-link\"?>";
-        foreach($radio->stream_url as $url) {
-            echo "\n<embed src=\"icy://$url\" autoplay=\"true\" />";
-        }
-        break;
-    case 'wax':
-        header("Content-Type: audio/x-ms-wax");
-        header("Content-Disposition: attachment; filename=$file->name.$file->ext");
-        foreach($radio->stream_url as $url) {
-            echo "$url\n";
-        }
-        break;
-    default:
-        break;
+if ($file->ext === 'asx') {
+    header("Content-Type: audio/x-ms-asf");
+    header("Content-Disposition: attachment; filename=$file->name.$file->ext");
+    echo "<ASX Version=\"3.0\">\n<PARAM name=\"HTMLView\" value=\"$radio->website_url\" />\n";
+    foreach($radio->stream_url as $url) {
+        echo "<ENTRY>\n<REF HREF=\"http://$url\" />\n</ENTRY>\n";
+    }
+    echo "<Title>$radio->stream_title</Title>\n</ASX>";
+} else if ($file->ext === 'm3u') {
+    header("Content-Type: audio/x-mpegurl");
+    header("Content-Disposition: attachment; filename=$file->name.$file->ext");
+    echo "#EXTM3U";
+    foreach($radio->stream_url as $url) {
+        echo "\n#EXTINF:-1, $radio->stream_title\n$url";
+    }
+} else if ($file->ext === 'pls') {
+    header("Content-Type: audio/x-scpls");
+    header("Content-Disposition: attachment; filename=$file->name.$file->ext");
+    echo "[playlist]\nNumberOfEntries=".count($radio->stream_url)."\n";
+    $i=0;
+    foreach($radio->stream_url as $url) {
+        $i++;
+        echo "\nFile$i=http://$url\nTitle$i=$radio->stream_title\nLength$i=-1\n";
+    }
+    echo "\nVersion=2";
+} else if ($file->ext === 'qtl') {
+    header("Content-Type: application/x-quicktimeplayer");
+    header("Content-Disposition: attachment; filename=$file->name.$file->ext");
+    echo "<?xml version=\"1.0\"?>\n<?quicktime type=\"application/x-quicktime-media-link\"?>";
+    foreach($radio->stream_url as $url) {
+        echo "\n<embed src=\"icy://$url\" autoplay=\"true\" />";
+    }
+} else if ($file->ext === 'wax') {
+    header("Content-Type: audio/x-ms-wax");
+    header("Content-Disposition: attachment; filename=$file->name.$file->ext");
+    foreach($radio->stream_url as $url) {
+        echo "$url\n";
+    }
 }
-?>
