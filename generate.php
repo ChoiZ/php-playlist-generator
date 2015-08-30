87fa57df60c06c4d82ff3d15a1c4ffda4e275ef3
François LASSERRE <choiz@me.com>
2015-08-30 14:41:17 +0200

    Rename radio to station

--- generate.php
+++ generate.php
@@ -9,21 +9,21 @@
 
 $available_format = array('asx','m3u','pls','qtl','wax');
 
-$radio = new stdClass;
-$radio->website_url = 'http://www.addictradio.net/';
-$radio->name = 'Addict Radio Rock';
-$radio->channel = 'addictrock';
-$radio->stream_url = array(
-    'stream1.addictradio.net/'.$radio->channel.'.mp3',
-    'stream2.addictradio.net/'.$radio->channel.'.mp3',
-    'stream3.addictradio.net/'.$radio->channel.'.mp3',
-    'stream4.addictradio.net/'.$radio->channel.'.mp3',
-    'stream5.addictradio.net/'.$radio->channel.'.mp3',
-    'stream6.addictradio.net/'.$radio->channel.'.mp3',
+$station = new stdClass;
+$station->website_url = 'http://www.addictradio.net/';
+$station->name = 'Addict Radio Rock';
+$station->channel = 'addictrock';
+$station->stream_url = array(
+    'stream1.addictradio.net/'.$station->channel.'.mp3',
+    'stream2.addictradio.net/'.$station->channel.'.mp3',
+    'stream3.addictradio.net/'.$station->channel.'.mp3',
+    'stream4.addictradio.net/'.$station->channel.'.mp3',
+    'stream5.addictradio.net/'.$station->channel.'.mp3',
+    'stream6.addictradio.net/'.$station->channel.'.mp3',
 );
 
 $playlist = new stdClass;
-$playlist->name = $radio->channel;
+$playlist->name = $station->channel;
 $playlist->ext = $available_format[1];
 
 if (!empty($_GET['ext']) && in_array(strtolower($_GET['ext']), $available_format)) {
@@ -33,39 +33,39 @@ if (!empty($_GET['ext']) && in_array(strtolower($_GET['ext']), $available_format
 if ($playlist->ext === 'asx') {
     header('Content-Type: audio/x-ms-asf');
     header('Content-Disposition: attachment; filename='.$playlist->name.$playlist->ext);
-    echo '<ASX Version="3.0">'."\n".'<PARAM name="HTMLView" value="'.$radio->website_url.'" />'."\n";
-    foreach($radio->stream_url as $url) {
+    echo '<ASX Version="3.0">'."\n".'<PARAM name="HTMLView" value="'.$station->website_url.'" />'."\n";
+    foreach($station->stream_url as $url) {
         echo '<ENTRY>'."\n".'<REF HREF="http://'.$url.'" />'."\n".'</ENTRY>'."\n";
     }
-    echo '<Title>'.$radio->name.'</Title>'."\n".'</ASX>';
+    echo '<Title>'.$station->name.'</Title>'."\n".'</ASX>';
 } else if ($playlist->ext === 'm3u') {
     header('Content-Type: audio/x-mpegurl');
     header('Content-Disposition: attachment; filename='.$playlist->name.$playlist->ext);
     echo '#EXTM3U';
-    foreach($radio->stream_url as $url) {
-        echo "\n".'#EXTINF:-1, '.$radio->name."\n".$url;
+    foreach($station->stream_url as $url) {
+        echo "\n".'#EXTINF:-1, '.$station->name."\n".$url;
     }
 } else if ($playlist->ext === 'pls') {
     header('Content-Type: audio/x-scpls');
     header('Content-Disposition: attachment; filename='.$playlist->name.$playlist->ext);
-    echo '[playlist]'."\n".'NumberOfEntries='.count($radio->stream_url)."\n";
+    echo '[playlist]'."\n".'NumberOfEntries='.count($station->stream_url)."\n";
     $i=0;
-    foreach($radio->stream_url as $url) {
+    foreach($station->stream_url as $url) {
         $i++;
-        echo "\n".'File'.$i.'=http://'.$url."\n".'Title'.$i.'='.$radio->name."\n".'Length'.$i.'=-1'."\n";
+        echo "\n".'File'.$i.'=http://'.$url."\n".'Title'.$i.'='.$station->name."\n".'Length'.$i.'=-1'."\n";
     }
     echo "\n".'Version=2';
 } else if ($playlist->ext === 'qtl') {
     header('Content-Type: application/x-quicktimeplayer');
     header('Content-Disposition: attachment; filename='.$playlist->name.$playlist->ext);
     echo '<?xml version="1.0"?>'."\n".'<?quicktime type="application/x-quicktime-media-link"?>';
-    foreach($radio->stream_url as $url) {
+    foreach($station->stream_url as $url) {
         echo "\n".'<embed src="icy://'.$url.'" autoplay="true" />';
     }
 } else if ($playlist->ext === 'wax') {
     header('Content-Type: audio/x-ms-wax');
     header('Content-Disposition: attachment; filename='.$playlist->name.$playlist->ext);
-    foreach($radio->stream_url as $url) {
+    foreach($station->stream_url as $url) {
         echo $url."\n";
     }
 }
